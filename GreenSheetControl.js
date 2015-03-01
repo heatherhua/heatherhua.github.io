bea79e9e636d94d9a6eb38e93a53e931317911a3
Heather Hua <heatherhua@Heathers-MacBook-Pro.local>
2015-03-01 15:26:18 -0800

    added array to fill rows to the same column...hopefully it works

--- GreenSheetControl.js
+++ GreenSheetControl.js
@@ -1,4 +1,28 @@
     var rows = 0;
+    var serviceHash = {
+    	2 : {
+    		"name" : "entry.479432008",
+    		"id" : "entry_479432008" ,
+        "title" : "Service Dates"
+    	},
+   		3 : {
+    		"name" : "entry.277624700",
+    		"id" : "entry_277624700" ,
+        "title" : "Service Events"
+    	},
+    	4 : {
+    		"name" : "entry.259088472",
+    		"id" : "entry_259088472" ,
+        "title" : "Service Hours"
+    	},
+    	5 : {
+    		"name" : "entry.1538896044",
+    		"id" : "entry_1538896044",
+        "title" : "Service Chairs" 
+    	}    	
+    }
+
+    // When rows > 5 we will convert the data to strings and send to paragraph for extra services.
 
     function addServiceRow() {
       // Find a <table> element with id="myTable":
@@ -13,22 +37,36 @@
       var cell4 = row.insertCell(3);
       var cell5 = row.insertCell(4);
 
-      // Add some text to the new cells:
+      // Number
       cell1.innerHTML = rows;
+      
+      // Date
       var when = document.createElement("input");
         when.setAttribute("type", "date");
-        when.setAttribute("name", "entry.479432008");
-        when.setAttribute("id", "entry_479432008");
-        when.setAttribute("aria-label", "Service #" +rows);
+        // Replace name and entry with array of ids
+        when.setAttribute("name", serviceHash[2].name);
+        when.setAttribute("id", serviceHash[2].id);
+        when.setAttribute("aria-label", serviceHash[2].title);
         when.setAttribute("aria-required", "true");
         when.setAttribute("class", "ss-q-date");
         when.setAttribute("dir", "auto");
 
       cell2.appendChild(when);
 
-      cell3.innerHTML = "NEW CELL3";
-      cell4.innerHTML = "NEW CELL4";
-      cell5.innerHTML = "NEW CELL5";
+      var text_cells = [cell3, cell4, cell5];
+
+      for (j = 0; j < text_cells.length; j++) {
+        var cell = j + 3;
+      	var data = document.createElement("input");
+      	data.setAttribute("type", "text");
+        data.setAttribute("name", serviceHash[cell].name);
+        data.setAttribute("id", serviceHash[cell].id);
+        data.setAttribute("aria-label", serviceHash[cell].title);
+        data.setAttribute("aria-required", "true");
+        data.setAttribute("class", "ss-q-short");
+        data.setAttribute("dir", "auto");
+      	text_cells[j].appendChild(data);
+      }
     };
 
     function loadServiceTable() {
